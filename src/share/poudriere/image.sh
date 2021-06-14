98ef8744c3acd1b3bf33c1fdc80eec23944b4ffc
Jose Luis Duran <jlduran@users.noreply.github.com>
2021-06-14 15:33:48 -0300

    Remove extra "/"

--- src/share/poudriere/image.sh
+++ src/share/poudriere/image.sh
@@ -391,7 +391,7 @@ post_getopts
 
 [ -n "${JAILNAME}" ] || usage
 
-: ${OUTPUTDIR:=${POUDRIERE_DATA}/images/}
+: ${OUTPUTDIR:=${POUDRIERE_DATA}/images}
 : ${IMAGENAME:=poudriereimage}
 MASTERNAME=${JAILNAME}-${PTNAME}${SETNAME:+-${SETNAME}}
 
